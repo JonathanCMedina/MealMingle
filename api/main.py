af0a37e54054f07f618b6206994d1d913cc9b654
Mitchell Mora <mitchell.andres.mora@gmail.com>
2023-08-17 23:46:12 +0000

    Mitchmerge1

--- api/main.py
+++ api/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import events, accounts
+from routers import events, accounts
 import os
 
 app = FastAPI()
 
 app.include_router(events.router)
-app.include_router(accounts.router)
+app.include_router(accounts.router, prefix="/api", tags=["accounts"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get("CORS_HOST", "http://localhost:3000")
-    ],
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,6 +26,6 @@ def launch_details():
             "week": 17,
             "day": 5,
             "hour": 19,
-            "min": "00"
+            "min": "00",
         }
     }
