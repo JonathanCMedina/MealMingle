e8916bc11d9f685c3f647268b1c2e056a9f94b46
Mitchell <mitchw36@gmail.com>
2023-09-08 19:16:56 -0700

    modified app.js browser router

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -9,26 +9,31 @@ import SignupForm from "./SignupForm.js";
 import EditEventPage from "./EditEvent.js";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 
-
+function GetToken() {
+  useToken();
+  return null;
+}
 
 function App() {
   const domain = /https:\/\/[^/]+/;
-  const basename = process.env.PUBLIC_URL.replace(domain, '')
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
   return (
     <div>
-        <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
-          <BrowserRouter basename={basename}>
-            <Routes>
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/signup" element={<SignupForm />} />
-              <Route path="/event" element={<EventForm />} />
-              <Route path="/events/:event_id/edit" element={<EditEventPage/>} />
-              {/* <Route path="/main" element={<MainApp />} /> This path currently has no js file linked */}
-              <Route path="/events" element={<EventsList />} />
-            </Routes>
-          </BrowserRouter>
+      {/* <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}> */}
+      <BrowserRouter basename={basename}>
+        <AuthProvider>
+          <GetToken />
+          <Routes>
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/event" element={<EventForm />} />
+            <Route path="/events/:event_id/edit" element={<EditEventPage />} />
+            {/* <Route path="/main" element={<MainApp />} /> This path currently has no js file linked */}
+            <Route path="/events" element={<EventsList />} />
+          </Routes>
         </AuthProvider>
+      </BrowserRouter>
     </div>
   );
 }
