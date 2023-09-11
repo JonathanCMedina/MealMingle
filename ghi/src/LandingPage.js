0af0c3aeb2ccf019665f39f63e081dd0c1e62cc9
Naomi <n.campos92@yahoo.com>
2023-09-11 10:57:30 -0700

    Remove redundant use of the word picture in an alt tag description

--- ghi/src/LandingPage.js
+++ ghi/src/LandingPage.js
@@ -4,7 +4,7 @@ import landingImage from './Images/landingImage.jpeg'
 const LandingPage = (props) => {
     return (
         <div className="landing-page p-7 relative w-100% h-100% overflow-hidden bg-cover bg-[100%] bg-no-repeat bg-black bg-center relative">
-            <img src={landingImage} className="w-full h-full object-cover" alt="Picture of a variety of people gathered around an outdoor table enjoying a meal together"/>
+            <img src={landingImage} className="w-full h-full object-cover" alt="A variety of people gathered around an outdoor table enjoying a meal together"/>
             <div className="pic-text-left">
                 <p className="p-5 text-white">Welcome to Meal Mingle, the place for mates, meals and merryment! Set a location, pick a theme and invite your closest friends to enjoy foods from around the world.</p>
                 <NavLink className="bg-yellow-500 m-5 p-3 rounded" to="/signup">Signup</NavLink>
