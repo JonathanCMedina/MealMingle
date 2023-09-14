041c2a7844ce75efa4de3af25c4896a253b6c33f
Jonathan Ceasar Medina <jonathan.c.medina.x@gmail.com>
2023-09-14 21:29:50 +0000

    Prettify stretch goals

--- ghi/src/EventsList.js
+++ ghi/src/EventsList.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 
 function EventsList(props) {
   const [eventsList, setEventsList] = useState([]);
   const { token } = useAuthContext();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!token) return;
@@ -30,29 +32,45 @@ function EventsList(props) {
     fetchEvents();
   }, [token]);
 
+  const handleEventClick = (eventId) => {
+    navigate(`/events/${eventId}`);
+  };
+
   return (
-    <div className=" min-h-screen bg-gray-200 dark:bg-gray-900">
+    <div className=" min-h-screen bg-gradient-to-b from-black to-green-800 dark:bg-gray-900">
       <main>
-        <h1 className="text-3xl pt-6 font-bold text-center"> All Events </h1>
+        <h1 className="text-3xl pt-6 text-gray-300 font-bold text-center">
+          {" "}
+          Welcome to all events{" "}
+        </h1>
         <div className="grid gap-10 lg:grid-cols-3 px-10">
           {eventsList?.map((prop) => {
             console.log("prop: ", prop);
             console.log("eventsList: ", eventsList);
             return (
               <div key={prop.event_id}>
-                <div className="gap-10 text-center shadow-md hover:shadow-green-500 shadow-yellow-500 p-6 rounded-xl my-10  dark:shadow-pink-700">
-                  <h3 className="text-lg pt-2 pb-2 dark:text-gray-200">
+                <div className="bg-black-500 gap-10 text-center text-white shadow-lg hover:shadow-green-500 shadow-yellow-500 p-6 rounded-xl my-10  dark:shadow-pink-700">
+                  <h3 className="text-xl pt-2 pb-2 dark:text-gray-300">
                     {prop.event_name}
                   </h3>
-                  <p className="dark:text-gray-300">Hosted by {prop.user_id}</p>
-                  <p className="py-2 text-green-800">
+                  <p className="text-lg pt-1 pb-1 dark:text-gray-300">
+                    Hosted by {prop.user_id}
+                  </p>
+                  <p className="py-2">
                     Located in {prop.address}, {prop.zipcode} on{" "}
                     {prop.event_date}
                   </p>
-                  <div className="text-gray-800 pb-1 dark:text-gray-300">
+                  <div className="pb-1 dark:text-gray-300">
                     <p className="pb-1"> {prop.description} </p>
                     <p>The featured cuisine is {prop.food_types}</p>
                   </div>
+                  <button
+                    onClick={() => handleEventClick(prop.event_id)}
+                    className="bg-yellow-500 hover:bg-green-500 text-black rounded-full px-3 py-1 text-md"
+                  >
+                    {" "}
+                    View Event Details{" "}
+                  </button>
                 </div>
               </div>
             );
